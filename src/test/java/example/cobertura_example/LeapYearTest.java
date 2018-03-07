267ff1987785971cd48b948f1de8ffa42e573096
Chihiro Iida <chihiro.iida7@gmail.com>
2018-03-07 20:48:12 +0900

    update

--- src/test/java/example/cobertura_example/LeapYearTest.java
+++ src/test/java/example/cobertura_example/LeapYearTest.java
@@ -1,9 +1,15 @@
 package example.cobertura_example;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 public class LeapYearTest {
+    
+    @Rule
+    public Timeout globalTimeout= new Timeout(20);
+    
     @Test
     public void test1() {
         Assert.assertTrue(new LeapYear().isLeapYear(4));
@@ -16,11 +22,19 @@ public class LeapYearTest {
     
     @Test
     public void test3() {
-        Assert.assertFalse(new LeapYear().isLeapYear(100));
+        Assert.assertTrue(new LeapYear().isLeapYear(100));
     }
     
     @Test
     public void test4() {
         Assert.assertFalse(new LeapYear().isLeapYear(1));
+    }
+    
+    @Test
+    public void test5() {
+        // time out
+        while(true) {
+            
+        }
     }
 }
